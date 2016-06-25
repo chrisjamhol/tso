dd62770617b006743802fd352f701e28d167b986
chrisjamhol <chrisjamhol@gmail.com>
2016-06-25 14:51:32 +0200

    added bootstrap grid system, first layout concept

--- wp-content/plugins/handball4all/handball4all.php
+++ wp-content/plugins/handball4all/handball4all.php
@@ -1,112 +1,160 @@
 <?php
-	/*
-	* Plugin Name: Handball4All - TSO
-	* Description: TSO Plugin für Spielergebnisse
-	* Version: 1.0
-	* Author: Christopher Holden
-	* Author URI: https://github.com/chrisjamhol
-	* Api Page: http://www.handball4all.de/index.php?tacoma=webpart.pages.Handball4allPage&navid=1263&coid=1263&cid=4&h4all=gfoi2i5k5nl7icfg5v4u8druo7 -- JSON Mannschaftsspielpläne: Übersicht der IDs und Links aktuelle Saison
-	*/
+    /*
+    * Plugin Name: Handball4All - TSO
+    * Description: TSO Plugin für Spielergebnisse
+    * Version: 1.0
+    * Author: Christopher Holden
+    * Author URI: https://github.com/chrisjamhol
+    * Api Page: http://www.handball4all.de/index.php?tacoma=webpart.pages.Handball4allPage&navid=1263&coid=1263&cid=4&h4all=gfoi2i5k5nl7icfg5v4u8druo7 -- JSON Mannschaftsspielpläne: Übersicht der IDs und Links aktuelle Saison
+    */
 
-	// Example 1 : WP Shortcode to display form on any page or post.
-	function handball4all_table($attributes){
-		$html = '';
+    // Example 1 : WP Shortcode to display form on any page or post.
+    /**
+     * @param $attributes
+     * @return string|void
+     */
+    function handball4all_table($attributes)
+    {
+        $html = '';
 
-		$parameters = shortcode_atts(getDefaultOptions(), $attributes );
-		//todo validate parameters
-		$teamData = getTeamData($parameters);		
-		$html = parseTeamData($teamData);
+        $parameters = shortcode_atts(getDefaultOptions(), $attributes);
+        //todo validate parameters
+        $teamData = getTeamData($parameters);
+        $html = parseTeamData($teamData);
 
-		return $html;
-	}
+        return $html;
+    }
 
-	function getDefaultOptions() {
-		$defaultOptions = array(			
-	        'baseurl' => 'http://www.handball4all.de/m/php/spo-proxy_public.php?cmd=data&',
-	        'manschaftsid' => 0,
-	        'quote' => 'quote',
-	        'attribution' => 'Author',
-		);
+    /**
+     * @return array
+     */
+    function getDefaultOptions()
+    {
+        $defaultOptions = array(
+            'baseurl' => 'http://www.handball4all.de/m/php/spo-proxy_public.php?cmd=data&',
+            'manschaftsid' => 0,
+            'quote' => 'quote',
+            'attribution' => 'Author',
+        );
 
-		return $defaultOptions;
-	}
+        return $defaultOptions;
+    }
 
-	function getTeamData($parameters) {
-		$cachefilename = plugin_dir_path(__FILE__).'cache/teamdata-'.$parameters['manschaftsid'].'.json';
-		$cacheIsOutdated = getIsCacheOutdated($cachefilename);
-		if($cacheIsOutdated)
-			createTeamDataCache($cachefilename, $parameters);
-		
-		$teamData = getCacheFromFile($cachefilename);	
+    /**
+     * @param $parameters
+     * @return array|mixed|object
+     */
+    function getTeamData($parameters)
+    {
+        $cachefilename = plugin_dir_path(__FILE__) . 'cache/teamdata-' . $parameters['manschaftsid'] . '.json';
+        $cacheIsOutdated = getIsCacheOutdated($cachefilename);
+        if ($cacheIsOutdated)
+            createTeamDataCache($cachefilename, $parameters);
 
-		return $teamData;
-	}
+        $teamData = getCacheFromFile($cachefilename);
 
-	function getTeamDataJson($parameters) {
-		$url = $parameters['baseurl'].'&lvTypeNext=team&lvIDNext='.$parameters['manschaftsid'];
-		$response = file_get_contents($url);
-		$teamData = json_decode($response, true);
+        return $teamData;
+    }
 
-		return $teamData;
-	}
+    /**
+     * @param $parameters
+     * @return array|mixed|object
+     */
+    function getTeamDataJson($parameters)
+    {
+        $url = $parameters['baseurl'] . '&lvTypeNext=team&lvIDNext=' . $parameters['manschaftsid'];
 
-	function parseTeamData($teamData) {		
-		include_once('partials/teamtable.php');
-	}
+        $response = file_get_contents($url);
+        $teamData = json_decode($response, true);
 
-	function getCacheFromFile($cachefilename) {
-		$cacheData = array();
+        return $teamData;
+    }
 
-		if(is_file($cachefilename)){
-			$fileContent = file_get_contents($cachefilename);
-			$cacheData = json_decode($fileContent);
-		}
-		
-		return $cacheData;
-	}
+    /**
+     * @param $teamData
+     */
+    function parseTeamData($teamData)
+    {
+        include_once('partials/teamtable.php');
+    }
 
-	function createTeamDataCache($cachefilename, $parameters) {
-		$teamDataJson = getTeamDataJson($parameters);
-		writeCacheFile($cachefilename, $teamDataJson);
-	}
+    /**
+     * @param $cachefilename
+     * @return array|mixed|object
+     */
+    function getCacheFromFile($cachefilename)
+    {
+        $cacheData = array();
 
-	function writeCacheFile($cachefilename, $jsonData) {	
-		if(is_writable($cachefilename)) {
-			$fp = fopen($cachefilename, 'w');
-			fwrite($fp, json_encode($jsonData));
-			fclose($fp);
-		} else {
-			echo $cachefilename.' not wirtable!';
-		}		
-	}	
+        if (is_file($cachefilename)) {
+            $fileContent = file_get_contents($cachefilename);
+            $cacheData = json_decode($fileContent);
+        }
 
-	function getIsCacheOutdated($cachefilename) {
-		$cacheIsOutdated = false;
+        return $cacheData;
+    }
 
-		if(is_file($cachefilename)) {
-			$cacheThreshold = 10 * 60; // 10 minutes
-			$currentTime = time();
-			$filetime = filemtime ($cachefilename);
-			$fileage = $currentTime - $filetime;
+    /**
+     * @param $cachefilename
+     * @param $parameters
+     */
+    function createTeamDataCache($cachefilename, $parameters)
+    {
+        $teamDataJson = getTeamDataJson($parameters);
+        writeCacheFile($cachefilename, $teamDataJson);
+    }
 
-			if($fileage > $cacheThreshold)
-				$cacheIsOutdated = true;
-		} else {			
-			$cacheIsOutdated = true;
-		}
+    /**
+     * @param $cachefilename
+     * @param $jsonData
+     */
+    function writeCacheFile($cachefilename, $jsonData)
+    {
+        if (is_writable($cachefilename)) {
+            $fp = fopen($cachefilename, 'w');
+            fwrite($fp, json_encode($jsonData));
+            fclose($fp);
+        } else {
+            echo $cachefilename . ' not wirtable!';
+        }
+    }
 
-		return $cacheIsOutdated = true;
-	}
+    /**
+     * @param $cachefilename
+     * @return bool
+     */
+    function getIsCacheOutdated($cachefilename)
+    {
+        $cacheIsOutdated = false;
 
-	function addResoureces() { 
-		wp_register_script('tso_script', plugins_url('javascript/tso.js', __FILE__), array('jquery'),'1.0', true);
-		wp_enqueue_script('tso_script');
+        if (is_file($cachefilename)) {
+            $cacheThreshold = 10 * 60; // 10 minutes
+            $currentTime = time();
+            $filetime = filemtime($cachefilename);
+            $fileage = $currentTime - $filetime;
 
-		wp_register_script('tso_stylesheet', plugins_url('css/tso.css', __FILE__));
-		wp_enqueue_script('tso_stylesheet');
-	}
+            if ($fileage > $cacheThreshold)
+                $cacheIsOutdated = true;
+        } else {
+            $cacheIsOutdated = true;
+        }
 
-	add_shortcode('handball4all-table', 'handball4all_table');
-	add_action( 'wp_enqueue_scripts', 'addResoureces' );
+        return $cacheIsOutdated = true;
+    }
+
+    /**
+     *
+     */
+    function addResoureces()
+    {
+        wp_register_script('tso_script', plugins_url('javascript/tso.js', __FILE__), array('jquery'), '1.0', true);
+        wp_enqueue_script('tso_script');
+
+        wp_register_style('tso_stylesheet', plugins_url('css/tso.css', __FILE__));
+        wp_enqueue_style('tso_stylesheet');
+    }
+
+    add_shortcode('handball4all-table', 'handball4all_table');
+    add_action('wp_enqueue_scripts', 'addResoureces');
 
 ?>
--- wp-content/plugins/handball4all/partials/teamtable.php
+++ wp-content/plugins/handball4all/partials/teamtable.php
@@ -1,21 +1,35 @@
 <div class="teamtable">
-	<table>	
-		<?php
-			if(!empty($teamData[0])):
-				$data = $teamData[0]->dataList;
+	<?php
+	if(!empty($teamData[0])):
+		$data = $teamData[0]->dataList;
 
-				foreach($data as $dataEntry):
-					print_r($dataEntry);
-					?>
-						<tr>
-							<td><?php echo $dataEntry->gHomeTeam ?></td>
-							<td><?php echo $dataEntry->gGuestTeam ?></td>
-							<td><?php echo $dataEntry->gHomeGoals ?></td>
-							<td><?php echo $dataEntry->gGuestGoals ?></td>
-						</tr>
-					<?php
-				endforeach;	
-			endif;
-		?>
-	</table>
+		foreach($data as $index => $dataEntry):?>
+			<div class="row <?php echo $index % 2 > 0 ? 'odd' : 'even'?>">
+
+				<div class="col-xs-12 teamtable-wrapper-main">
+
+					<div class="row">
+						<div class="col-xs-8">
+							<strong><?php echo $dataEntry->gHomeTeam ?> - <?php echo $dataEntry->gGuestTeam ?></strong><br />
+							<?php echo $dataEntry->gDate ?> <?php echo $dataEntry->gTime ?>
+						</div>
+						<div class="col-xs-4 text-right">
+							<?php echo $dataEntry->gHomeGoals ?>:<?php echo $dataEntry->gGuestGoals ?>
+							<?php if($dataEntry->gHomeGoals_1 > 0 || $dataEntry->gGuestGoals_1 > 0) {?>
+								(<?php echo $dataEntry->gHomeGoals_1 ?>:<?php echo $dataEntry->gGuestGoals_1 ?>)
+							<?php } ?>
+						</div>
+					</div>
+
+					<div class="row">
+
+					</div>
+				</div>
+				<div class="col-xs-12 teamtable-wrapper-info">
+
+				</div>
+			</div>
+		<?php endforeach;
+	endif;
+	?>
 </div>
