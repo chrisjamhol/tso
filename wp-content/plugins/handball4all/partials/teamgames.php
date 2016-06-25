2c9a83a808b09e984a31b8e974bab31a8ed73326
chrisjamhol <chrisjamhol@gmail.com>
2016-06-25 16:06:27 +0200

    responsive teamgames table

--- wp-content/plugins/handball4all/partials/teamgames.php
+++ wp-content/plugins/handball4all/partials/teamgames.php
@@ -2,11 +2,17 @@
     <table class="tablepress ">
         <thead>
             <tr>
-                <th>Datum</th>
-                <th>Heim</th>
-                <th>Gast</th>
-                <th>Halbzeit</th>
-                <th>Ergebnis</th>
+                <th class="hidden-xs">Datum</th>
+                <th>
+                    <span class="hidden-xs">Heim</span>
+                    <span class="visible-xs">Manschaften</span>
+                </th>
+                <th>
+                    <span class="hidden-xs">Gast</span>
+                    <span class="visible-xs">Info</span>
+                </th>
+                <th class="hidden-xs">Halbzeit</th>
+                <th class="hidden-xs">Ergebnis</th>
             </tr>
         </thead>
         <tbody>
@@ -23,15 +29,30 @@
                         continue;
                 ?>
                     <tr class="<?php echo $index % 2 > 0 ? 'odd' : 'even'?>">
-                        <td><?php echo $dataEntry->gDate ?><br /><?php echo $dataEntry->gTime ?></td>
-                        <td><?php echo $dataEntry->gHomeTeam ?></td>
-                        <td><?php echo $dataEntry->gGuestTeam ?></td>
+                        <td class="hidden-xs">
+                            <?php echo $dataEntry->gDate ?><br /><?php echo $dataEntry->gTime ?> Uhr
+                        </td>
                         <td>
+                            <?php echo $dataEntry->gHomeTeam ?>
+                            <span class="visible-xs">
+                                <?php echo $dataEntry->gGuestTeam ?>
+                            </span>
+                        </td>
+                        <td>
+                            <span class="hidden-xs"><?php echo $dataEntry->gGuestTeam ?></span>
+                            <span class="visible-xs">
+                                <strong>Ergebnis: <?php echo $dataEntry->gHomeGoals ?>:<?php echo $dataEntry->gGuestGoals ?></strong><br />
+                                <?php echo $dataEntry->gDate ?><br /><?php echo $dataEntry->gTime ?> Uhr
+                            </span>
+                        </td>
+                        <td class="hidden-xs">
                             <?php if($dataEntry->gHomeGoals_1 > 0 || $dataEntry->gGuestGoals_1 > 0) { ?>
                                 <?php echo $dataEntry->gHomeGoals_1 ?>:<?php echo $dataEntry->gGuestGoals_1 ?>
                             <?php } ?>
                         </td>
-                        <td><?php echo $dataEntry->gHomeGoals ?>:<?php echo $dataEntry->gGuestGoals ?></td>
+                        <td class="hidden-xs">
+                            <?php echo $dataEntry->gHomeGoals ?>:<?php echo $dataEntry->gGuestGoals ?>
+                        </td>
                     </tr>
                 <?php endforeach;
             endif;
